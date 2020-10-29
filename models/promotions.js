c4b58f5a8252aad25e652b31a9b4e95cab44b998
korlahwarleh <jmkolawole@gmail.com>
2020-10-29 20:00:23 +0100

    first commit

---
+++ models/promotions.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+
+const promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default:'' 
+    },
+    price: {
+        type: Currency,
+        required: true
+
+    },
+    featured :{
+        type : Boolean,
+        required: true
+    },
+    description : {
+        type: String,
+        required : true
+    }
+  },{
+      timestamps : true
+  }
+  );
+  
+
+  
+var Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;
